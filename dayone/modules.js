4d42460d4a78dc6935d3cb75e77a392bd106fbc5
Aravind V V <arvndvv@gmail.com>
2020-11-28 13:14:14 +0530

    export multiple arays as object

--- dayone/modules.js
+++ dayone/modules.js
@@ -1,3 +1,4 @@
 const People = require('./people');
 console.log(People);
 // console.log(people)
+console.log(People.ages);
--- dayone/people.js
+++ dayone/people.js
@@ -1,4 +1,5 @@
 const people = ["Arvndvv", "b374br4in", "Butcher", "william", "homelander", "hughie"]
 console.log(people);
-
-module.exports = people;
+const ages = [22, 22, 24, 30, 18];
+// multiple export
+module.exports = { people, ages };
