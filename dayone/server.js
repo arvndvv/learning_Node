990cde57c28e2c6e5d0dc488855c616c524a6cda
Aravind V V <arvndvv@gmail.com>
2020-11-28 16:42:40 +0530

    send html file with req

--- dayone/server.js
+++ dayone/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     // console.log('request made');
     console.log(req.url, req.method);
@@ -8,8 +8,21 @@ const server = http.createServer((req, res) => {
     // res.write("helo,there");
     // res.end();
     res.setHeader("Content-Type", "text/html");
-    res.write('<p>hi</p><h1>biggie</h1>')
-    res.end();
+    //send html file
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            // res.end();
+            //if we want to send only one data, we can directly send data through res.end
+            res.end(data)
+        }
+
+    })
+
 
 });
 server.listen(2999, 'localhost', () => {
