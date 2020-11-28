3c716355109f8b7d484dba274b5d0d32518e09d1
Aravind V V <arvndvv@gmail.com>
2020-11-28 12:58:38 +0530

    settimeout,setinterval

--- dayone/test.js
+++ dayone/test.js
@@ -1,6 +1,16 @@
-// const name = "";
-const greet = name => {
-    console.log(`helo,${name}`);
-}
+// // const name = "";
+// const greet = name => {
+//     console.log(`helo,${name}`);
+// }
 
-greet("aravind")
+// greet("aravind")
+// console.log(global)
+setTimeout(() => {
+    console.log("timeout")
+}, 5000);
+const int = setInterval(() => {
+    console.log("interval");
+
+    clearInterval(int);
+
+}, 1000);
