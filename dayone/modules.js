4417624fe6fcafaedc275c0ed6ea6f98db5350e7
Aravind V V <arvndvv@gmail.com>
2020-11-28 13:18:32 +0530

    os module

--- dayone/modules.js
+++ dayone/modules.js
@@ -1,4 +1,6 @@
 const { people, ages } = require('./people');
-console.log(people);
+// console.log(people, ages);
 // console.log(people)
-console.log(ages);
+// console.log(ages);
+const os = require('os');
+console.log(os.platform(), os.homedir());
