8c04b16c882eed0c76dc74e3182ab22b9fa43104
Aravind V V <arvndvv@gmail.com>
2020-11-28 13:00:59 +0530

    __dirname and __filename

--- dayone/test.js
+++ dayone/test.js
@@ -5,12 +5,17 @@
 
 // greet("aravind")
 // console.log(global)
-setTimeout(() => {
-    console.log("timeout")
-}, 5000);
-const int = setInterval(() => {
-    console.log("interval");
+//global.setTimeout
+// setTimeout(() => {
+//     console.log("timeout");
+//     clearInterval(int); //clear interval
+// }, 5000); //calls after 5s
+// const int = setInterval(() => {
+//     console.log("interval");
 
-    clearInterval(int);
 
-}, 1000);
+
+// }, 1000); //repeats every 1s
+
+console.log(__dirname);
+console.log(__filename)
