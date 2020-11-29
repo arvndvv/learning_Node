399b5e36bee982ef78d49858c39a60f00cf985bb
Aravind V V <arvndvv@gmail.com>
2020-11-29 09:24:56 +0530

    sending file as response using express

--- dayone/app.js
+++ dayone/app.js
@@ -6,8 +6,9 @@ const app = express();
 
 app.listen(3000);
 app.get('/', (req, res) => {
-    res.send('<p>Yo Home!</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
 })
 app.get('/about', (req, res) => {
-    res.send('<p>Yo About!</p>');
+
+    res.sendFile('./views/about.html', { root: __dirname });
 })
