c9affc7962b891a1d01ee3bb001889daf06642f1
Aravind V V <arvndvv@gmail.com>
2020-11-29 09:50:50 +0530

    redirects and 404 in express

--- dayone/app.js
+++ dayone/app.js
@@ -12,3 +12,16 @@ app.get('/about', (req, res) => {
 
     res.sendFile('./views/about.html', { root: __dirname });
 })
+
+//redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+})
+
+// use is fired at every req, but if req matches something before, say about, or index, then express wont reach executing use.
+// we should place app.use at last
