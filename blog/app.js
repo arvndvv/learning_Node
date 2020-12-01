0f242912d8f24032edc58dab8cdba46ef14f1d5e
Aravind V V <arvndvv@gmail.com>
2020-12-01 23:40:21 +0530

    blog

--- blog/app.js
+++ blog/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 //morgan middleware
 // const morgan = require('morgan')
 const mongoose = require('mongoose');
-const Blog = require("./models/blog")
-    //express app
+const blogRoutes = require('./routes/blogRoutes');
+
+//express app
 const app = express();
 //connect to mongodb
 const dbURI = 'mongodb+srv://betabrain:beta1234@blog.wo1vw.mongodb.net/blogtut?retryWrites=true&w=majority';
@@ -37,64 +38,8 @@ app.get('/about', (req, res) => {
         // res.sendFile('./views/about.html', { root: __dirname });
         res.render('about', { title: 'About' });
     })
-    //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-//taking post request data and sending it to  blog model
-app.post('/blogs', (req, res) => {
-    // console.log(req.body)
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch(err => {
-            console.log(err)
-        })
-
-})
-
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create Blog' });
-})
-
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: result.title })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-
-            res.json({ redirect: '/blogs', msg: 'Blog Trashed!' })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
-//redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
-
+    //blog Routes
+app.use('/blogs', blogRoutes);
 // 404 page
 app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', { root: __dirname });
